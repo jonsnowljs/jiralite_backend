a8470db7b22062756b3e971527fa9a8e2e805605
Jiasheng Lu <1473875731@qq.com>
2022-06-09 23:50:45 -0600

    chore: update index.js

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema/schema")
 const connectDB = require('./config/db')
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // connect to mongoDB
 connectDB()
+
+app.use(cors())
 
 app.use(
   '/graphql',
